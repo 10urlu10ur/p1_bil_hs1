27f63dd8b75b6bf31d53cbfeb46f888a4e569483
10urlu10ur <153202301+10urlu10ur@users.noreply.github.com>
2023-12-07 14:49:25 +0000

    exexefxethbyj

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print('Hello World2')
+# print('rastgele\nbir\nşey')
+# print("Merhaba "+"Şahin")
+# print("Merhaba "+input("Adın ne? "))
+# print(len(input("Merhaba, Adın nedir?")))
+# a = input("Adın ne? ")
+# print(a)
+# ad = "Onur"
+# print(ad)
+# ad = "Erdem"
+# print(ad)
+# ad = input("Adın ne? ")
+# uzunluk = len(ad)
+# print(uzunluk)
+sayı1 = input("İlk sayıyı giriiniz")
+sayı2 = input("İkinci sayıyı giriiniz")
+sayı3 = sayı1
+sayı1 = sayı2
+sayı2 = sayı3
+print("İlk sayı: "+sayı1)
+print("İkinci sayı: "+sayı2)
